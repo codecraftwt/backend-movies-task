7a9177d7037fd3a0f879964645ac25c5b62cd72f
codecraftwt <codecraftwt@gmail.com>
2024-11-18 11:55:40 +0530

    changed config

--- config/cloudinaryConfig.js
+++ config/cloudinaryConfig.js
@@ -3,10 +3,15 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 
+// cloudinary.config({
+//   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+//   api_key: process.env.CLOUDINARY_API_KEY,
+//   api_secret: process.env.CLOUDINARY_API_SECRET,
+// });
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
+  cloud_name: 'dwwykeft2',
+  api_key: '171225899953946',
+  api_secret: 'cNv0XKbJqqg10440xozFTLZVrrk',
 });
 
 module.exports = cloudinary;
